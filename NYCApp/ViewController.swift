6fb0dec0c3373ae52ce19088a331f7eb35156fe6
antoinettetorr <70113587+antoinettetorr@users.noreply.github.com>
2021-07-21 01:39:43 -0400

    Initial Commit

---
+++ NYCApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NYCApp
+//
+//  Created by Antoinette Marie Torres on 7/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logInLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func userInput(_ sender: Any) {
+    }
+    @IBAction func passInput(_ sender: Any) {
+    }
+    @IBAction func joinTapped(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ NYCApp/homepageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  homepageViewController.swift
+//  NYCApp
+//
+//  Created by Antoinette Marie Torres on 7/21/21.
+//
+
+import UIKit
+
+class homepageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func profileTapped(_ sender: UIButton) {
+    }
+    @IBAction func chatTapped(_ sender: UIButton) {
+    }
+    @IBAction func homeTapped(_ sender: UIButton) {
+    }
+    @IBAction func mapTapped(_ sender: UIButton) {
+    }
+    @IBAction func resourcesTapped(_ sender: UIButton) {
+    }
+    
+}
