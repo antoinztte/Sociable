7ebbbc46e365b042ffc419cd05782aeceeb6f2a9
sofiavcobos <sofiavcobos@gmail.com>
2021-07-22 11:43:40 -0400

    added resource page

--- NYCApp/ProfileViewController.swift
+++ NYCApp/ProfileViewController.swift
@@ -7,20 +7,43 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var profileLabel: UIView!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var locationText: UITextField!
     @IBOutlet weak var birthdayText: UITextField!
     @IBOutlet weak var bioText: UITextField!
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func takeSelfieTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func photoLibraryTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
 
+    
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info : [UIImagePickerController.InfoKey : Any]){
+                
+                if let selectedImage = info [UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                    displayImage.image = selectedImage
+                }
+                imagePicker.dismiss(animated: true, completion: nil)
+            }
+    
     @IBAction func saveButton(_ sender: UIButton) {
         
         
